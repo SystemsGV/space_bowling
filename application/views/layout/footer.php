788bd5f0a1b42b384b89b9159414ff485b88d5ed
Emerson Valenzuela <144744129+SystemsGV@users.noreply.github.com>
2024-05-24 09:47:39 -0500

    :art: Added Icon IG

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -23,6 +23,7 @@
                         </a>
                         <div class="footer-icons">
                             <a href="https://www.facebook.com/cosmicbowling1/" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+                            <a target="_blank" href="https://www.instagram.com/cosmic_bowling/" title="Instagram"><i class="fa fa-instagram"></i></a>
                             <a target="_blank" href="https://www.tiktok.com/@cosmic_bowling?is_from_webapp=1&sender_device=pc" title="Instagram"><i class="fa-brands fa-tiktok"></i></a>
                         </div>
                     </div>
