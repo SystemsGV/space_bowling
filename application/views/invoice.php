77754387c23f6a69d74e6c1d0ea2fd55a2bc3bad
Emerson Valenzuela <144744129+SystemsGV@users.noreply.github.com>
2024-05-30 17:25:24 -0500

    :memo: Modify tag titlew

--- application/views/invoice.php
+++ application/views/invoice.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>CosmicBowling | Solicitud de Comprobante</title>
 
   <link rel="stylesheet" href="<?= base_url() ?>new/vendor/flatpickr/flatpickr.css">
   <link rel="stylesheet" href="<?= base_url() ?>new/invoice/invoice.css">
