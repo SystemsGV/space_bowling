7184e20e2759804b1b2eba635b9dd343326d6683
SystemsGV <jsistemasgv@gmail.com>
2024-12-12 16:48:10 -0500

    :rocket: Email and PDF succes!!

--- application/controllers/SiteCoupons.php
+++ application/controllers/SiteCoupons.php
@@ -138,14 +138,14 @@ class SiteCoupons extends CI_Controller
 				$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
 				$mail->isSMTP();                                            //Send using SMTP
 				$mail->Host       = 'mail.cosmicbowling.com.pe';                     //Set the SMTP server to send through
-				$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+				$mail->SMTPAuth   = false;                                   //Enable SMTP authentication
 				$mail->Username   = 'sistemas@cosmicbowling.com.pe';                     //SMTP username
 				$mail->Password   = '3JvZBRlUpilP';                               //SMTP password
-				$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+				// $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
 				$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
 				//Recipients
-				$mail->setFrom($from);
+				$mail->setFrom($from, $name);
 				$mail->addAddress($to, $name);               //Name is optional
 				// $mail->addReplyTo('info@example.com', 'Information');
 				// $mail->addCC('cc@example.com');
